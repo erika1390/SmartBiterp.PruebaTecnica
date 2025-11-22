a773c15f3bbd86471960320c118d8bc9153726ef
epulido <erika1390@gmail.com>
2025-11-22 01:16:27 -0500

    Request

---
+++ SmartBiterp.Application/DTOs/Expense/CreateExpenseTypeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartBiterp.Application.DTOs.Expense
+{
+    public class CreateExpenseTypeRequest
+    {
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+}
