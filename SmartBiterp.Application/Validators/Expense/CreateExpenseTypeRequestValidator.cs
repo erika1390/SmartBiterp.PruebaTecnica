fb92f7cc85f031a85039ca97ea656613ed58f835
epulido <erika1390@gmail.com>
2025-11-22 01:16:09 -0500

    Validadores

---
+++ SmartBiterp.Application/Validators/Expense/CreateExpenseTypeRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+using SmartBiterp.Application.DTOs.Expense;
+
+namespace SmartBiterp.Application.Validators.Expense
+{
+    public class CreateExpenseTypeRequestValidator : AbstractValidator<CreateExpenseTypeRequest>
+    {
+        public CreateExpenseTypeRequestValidator()
+        {
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(100);
+
+            RuleFor(x => x.Category)
+                .NotEmpty().WithMessage("Category is required.");
+        }
+    }
+}
---
+++ SmartBiterp.Application/Validators/Expense/UpdateExpenseTypeRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+using SmartBiterp.Application.DTOs.Expense;
+
+namespace SmartBiterp.Application.Validators.Expense
+{
+    public class UpdateExpenseTypeRequestValidator : AbstractValidator<UpdateExpenseTypeRequest>
+    {
+        public UpdateExpenseTypeRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Invalid id.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Category)
+                .NotEmpty();
+        }
+    }
+}
